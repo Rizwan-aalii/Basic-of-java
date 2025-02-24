44e9016d5441d5db6dc9fa7579dcdc13b0f86179
Rizwan-aalii <mrn892301@gmail.com>
2025-02-24 20:25:25 +0530

    arrays

---
+++ alllecture/classsix.java
@@ -0,0 +1,29 @@
+package alllecture;
+
+public class classsix {
+    // arrys
+    public static void main (String args []){
+        int [] price = new int [3];
+        price [0]= 100;
+        price [1] = 150;
+        price [2] = 60 ;
+        
+        
+        for(int i = 0 ; i<3 ; i++){
+            System.out.println(price[i]);
+        }
+
+        int num [] = {10 , 15 , 14 };
+        for(int j = 0 ; j<3 ; j++){
+            System.out.println(num[j]);
+        }
+    }
+}
+
+
+
+
+
+
+
+
