29f62b1ec028e23a34d4ef81f18f8bd970321ba7
Rizwan-aalii <mrn892301@gmail.com>
2025-04-09 20:56:19 +0530

    practice like hell

--- ArrayPractice/Array1.java
+++ ArrayPractice/Array1.java
@@ -1,5 +1,43 @@
 package ArrayPractice;
+import java.lang.reflect.Array;
 import java.util.*;
+
+class InputWithArray {
+    public void  InputArray () {
+        Scanner sc = new Scanner(System.in);
+      System.out.println("Enter Your Array Size ");
+      int size = sc.nextInt();
+     int Arru [] = new int[size];
+    System.out.println("Enter your Array Value ");
+     for(int i = 0 ; i<size ; i++){
+        Arru [i] = sc.nextInt();
+    }
+    System.out.println(Arrays.toString(Arru));
+    for(int i = 0 ; i<Arru.length / 2 ; i++){
+        int temp = Arru[i];
+        Arru[i] = Arru[Arru.length - 1 - i];
+        Arru[Arru.length - 1 - i] = temp;
+    }
+    System.out.println(Arrays.toString(Arru));
+
+
+    }
+
+
+}
+class Array2 {
+     public void MyArray () {
+        int Arraysa [] = {11,12,15,16,18,19};
+        System.out.println(Arrays.toString(Arraysa));
+        for(int i = 0 ; i<Arraysa.length / 2 ; i++){
+            int temp = Arraysa[i];
+            Arraysa[i] = Arraysa[Arraysa.length - 1 - i];
+            Arraysa[Arraysa.length - 1 -i ] = temp ;
+        }
+        System.out.println(Arrays.toString(Arraysa));
+        
+    }
+}
 
 public class Array1 {
     public static void main(String[] args) {
@@ -35,7 +73,16 @@ public class Array1 {
         }
         System.out.println(Arrays.toString(Array));
         
+        System.out.println( "Array Length " + Array.length / 2 );
+        System.out.println( "Array Length " + Array.length % 2 );
+        Array2 arr = new Array2();
+        arr.MyArray();
+        InputWithArray arru = new InputWithArray();
+        arru.InputArray();
 
         
+
+
     }
+
 }
